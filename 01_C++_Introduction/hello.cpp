f413cd248071404e96ba23a148ec119fbeead129
Naser <sir.naser10@proton.me>
2025-12-06 00:04:25 +0330

    <3

---
+++ 01_C++_Introduction/hello.cpp
@@ -0,0 +1,17 @@
+// include iostream instead of stdio.h
+#include <iostream>
+
+// specify namespace: std
+using namespace std;
+
+int main(){
+    int a, b;
+
+    // using cin instead of scanf
+    cin >> a >> b;
+
+    // using cout instead of printf
+    cout << "a: " << a << ", b: " << b << endl;
+    cout << "a+b: " << a+b << endl;
+    cout << "hello from C++\n";
+}
